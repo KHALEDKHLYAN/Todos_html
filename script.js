ea76d87c4b4dd9cbffd1e5463e8f231500aeff98
REREU LEMEIN <95699811+KHALEDKHLYAN@users.noreply.github.com>
2023-02-24 10:56:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// Get references to the form, input field, and unordered list
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUL = document.getElementById("todos");
 
+// Retrieve the todos from local storage and display them on the page
 const todos = JSON.parse(localStorage.getItem("todos"));
 
 if (todos) {
@@ -10,12 +12,14 @@ if (todos) {
     });
 }
 
+// Add a listener to the form to handle adding new todos
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     addTodo();
 });
 
+// Define a function to add a new todo to the list
 function addTodo(todo) {
     let todoText = input.value;
 
@@ -24,13 +28,18 @@ function addTodo(todo) {
     }
 
     if (todoText) {
+        // Create a new list item element for the todo
         const todoEl = document.createElement("li");
+
+        // If the todo is already completed, add a class to the element
         if (todo && todo.completed) {
             todoEl.classList.add("completed");
         }
 
+        // Set the text content of the element to the todo text
         todoEl.innerText = todoText;
 
+        // Add event listeners to toggle the completed class and delete the todo
         todoEl.addEventListener("click", () => {
             todoEl.classList.toggle("completed");
 
@@ -45,19 +54,24 @@ function addTodo(todo) {
             updateLS();
         });
 
+        // Append the new todo element to the unordered list
         todosUL.appendChild(todoEl);
 
+        // Clear the input field
         input.value = "";
 
+        // Update local storage with the new todo
         updateLS();
     }
 }
 
+// Define a function to update local storage with the current list of todos
 function updateLS() {
     const todosEl = document.querySelectorAll("li");
 
     const todos = [];
 
+    // Iterate over each todo element and add its text and completed status to the todos array
     todosEl.forEach((todoEl) => {
         todos.push({
             text: todoEl.innerText,
@@ -65,5 +79,6 @@ function updateLS() {
         });
     });
 
+    // Save the todos array to local storage as a JSON string
     localStorage.setItem("todos", JSON.stringify(todos));
 }
